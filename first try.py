d2dfc313724e4ddfe6cc9341273fa75367265c03
vincent <vincegams24@gmail.com>
2025-06-18 22:16:23 -0700

    first commit

---
+++ first try.py
@@ -0,0 +1,8 @@
+print("Simple calculator/n")
+print("press 1 for addtion and press 2 for subtractio")
+
+chose = input("Chose 1 or two: ")
+
+if chose in ('1', '2'):
+    num2 = float(input("enter number: "))
+    num1 = float(input("enter number2: "))
