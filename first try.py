7df7ccc608f003201bde0ef1497f0a99ee493632
vincent <vincegams24@gmail.com>
2025-06-19 20:57:04 -0700

    practice

--- first try.py
+++ first try.py
@@ -1,8 +1,13 @@
 print("Simple calculator/n")
 print("press 1 for addtion and press 2 for subtractio")
 
-chose = input("Chose 1 or two: ")
+chose = input("Chose 1 or 2: ")
 
 if chose in ('1', '2'):
-    num2 = float(input("enter number: "))
-    num1 = float(input("enter number2: "))
+    num1 = float(input("enter number: "))
+    num2 = float(input("enter number2: "))
+
+    if chose == '1':
+        print(f"{num1} + {num2} = {num1 + num2}")
+    if chose =='2':
+        print(f"{num1} - {num2} = {num1 - num2}")
