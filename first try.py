e85ba386d888a1079ddf122d3a0c3ad23ae1696c
vincent <vincegams24@gmail.com>
2025-06-20 22:15:09 -0700

    division

--- first try.py
+++ first try.py
@@ -1,9 +1,9 @@
 print("Simple calculator/n")
 print("press 1 for addtion and press 2 for subtractio")
 
-chose = input("Chose 1 or 2: ")
+chose = input("Chose 1, 2, 3, 4: ")
 
-if chose in ('1', '2'):
+if chose in ('1', '2', '3', '4'):
     num1 = float(input("enter number: "))
     num2 = float(input("enter number2: "))
 
@@ -11,3 +11,5 @@ if chose in ('1', '2'):
         print(f"{num1} + {num2} = {num1 + num2}")
     if chose =='2':
         print(f"{num1} - {num2} = {num1 - num2}")
+    if chose == '3':
+        print(f"{num1} / {num2} = {num1 / num2}")
